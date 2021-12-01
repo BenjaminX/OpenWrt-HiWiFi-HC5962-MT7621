aab568a09abaa05df2559477ba9a6d7a316946fd
Benjamin <benjaminx@gmail.com>
2021-12-01 09:20:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo '### Updates default IP gate ###'
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+echo '###  ###'
+
+echo '### Updates Theme Argon ###'
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+echo '###  ###'
