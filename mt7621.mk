da4037aac14171a1929aefb57d4b871649d42ac6
Benjamin <benjaminx@gmail.com>
2021-12-06 10:12:16 +0800

    Update mt7621.mk

--- mt7621.mk
+++ mt7621.mk
@@ -678,7 +678,7 @@ define Device/hiwifi_hc5962
   PAGESIZE := 2048
   KERNEL_SIZE := 4096k
   UBINIZE_OPTS := -E 5
-  IMAGE_SIZE := 65536k
+  IMAGE_SIZE := 121072k
   IMAGES += factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
