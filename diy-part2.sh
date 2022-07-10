b48e205239ec361108ca301f298383cf3724dfda
Benjamin <benjaminx@gmail.com>
2022-07-10 16:20:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,9 +15,10 @@ echo '### Updates default IP gate ###'
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 echo '###  ###'
 
-echo '### Updates Theme Argon ###'
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# echo '### Updates Theme Argon ###'
+# package/lean/luci-theme-argon /package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/luci/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
 echo '###  ###'
 
 echo '### 添加 mt7621 支持 ###'
